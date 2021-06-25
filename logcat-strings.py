d23a3006c4bd5f92d21f5da83dd925a2058597ee
stark0de <53110298+stark0de@users.noreply.github.com>
2021-06-25 20:21:13 +0200

    Update logcat-strings.py

--- logcat-strings.py
+++ logcat-strings.py
@@ -14,12 +14,15 @@ print("Device info: ")
 print("Arch: "+device.arch)
 print("Version: "+device.product)
 packages=adb.adb(['shell','pm', 'list', 'packages']).decode('utf-8').split("\n")
+packagenames=[]
 for package in packages:
     if appname.lower() in package.lower():
        print("[+] Possible package name: "+package.lower().split(":")[1])
-
+       packagenames.append(package.lower().split(":")[1])
 print("===============================================")
-commonstrings=["pwd", "password", "hash", "key", "API key", "code", "2FA", "passwd", "pin", "pincode", "@", "secret", "private", "serial", "logon", "login", "logged", "session", "creds", "credential"]
+if len(packagenames) == 1:
+   appname=packagenames[0]
+commonstrings=["pwd", "password", "hash", "key", "API key", "api","code", "2FA", "passwd", "pin", "pincode", "@", "secret", "private", "serial", "logon", "login", "logged", "session", "creds", "credential"]
 lines=[]
 while True:
       x = adb.logcat().decode('utf-8').split("\n")
