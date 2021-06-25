72b9caa5c33896a4b9637874c2aa48df2412a708
stark0de <53110298+stark0de@users.noreply.github.com>
2021-06-25 20:17:37 +0200

    Create logcat-strings.py

---
+++ logcat-strings.py
@@ -0,0 +1,33 @@
+from pwn import *
+import sys
+
+if len(sys.argv) != 2:
+    print("Usage: python3 logcat-strings.py packagenameorappname")
+    sys.exit()
+
+appname=sys.argv[1]
+
+device = adb.wait_for_device()
+print("Simple tool to check for interesting strings in ADB logs")
+print("===============================================")
+print("Device info: ")
+print("Arch: "+device.arch)
+print("Version: "+device.product)
+packages=adb.adb(['shell','pm', 'list', 'packages']).decode('utf-8').split("\n")
+for package in packages:
+    if appname.lower() in package.lower():
+       print("[+] Possible package name: "+package.lower().split(":")[1])
+
+print("===============================================")
+commonstrings=["pwd", "password", "hash", "key", "API key", "code", "2FA", "passwd", "pin", "pincode", "@", "secret", "private", "serial", "logon", "login", "logged", "session", "creds", "credential"]
+lines=[]
+while True:
+      x = adb.logcat().decode('utf-8').split("\n")
+      #print(type(x))
+      for line in x:
+          line=line.lower()
+          if appname.lower() in line:
+             for i in commonstrings:
+                 if i in line and line not in lines:
+                    lines.append(line)
+                    print("Match "+i+" :"+line)
